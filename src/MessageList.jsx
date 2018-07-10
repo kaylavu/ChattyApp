f3d08fe62b3f55990c332a2beba7df6a7e82ef4e
kaylavu <kaylaavu@gmail.com>
2018-07-10 20:37:51 +0000

    render messages

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import Message from './Message.jsx'
 
-export default function MessageList() {
+export default function MessageList(props) {
+
+
+    const messages = props.messages.map((message) => {
+        return <Message key={message.id} userName={message.username} content={message.content} />
+    });
+
+
     return (
-  <main className="messages">
-   <Message/>
-    <div className="message system">
-      Anonymous1 changed their name to nomnom.
-    </div>
-  </main>
+        <main className="messages">
+            {messages}
+            <div className="message system">
+
+            </div>
+        </main>
     )
-  }
+}
