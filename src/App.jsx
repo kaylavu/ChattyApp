cdb7efb3fa79e246b5a07acdec13383e2922ab9f
kaylavu <kaylaavu@gmail.com>
2018-07-11 20:29:20 +0000

    user can send to webSocket

--- src/App.jsx
+++ src/App.jsx
@@ -31,28 +31,23 @@ class App extends Component {
 
   }
 
+
+
   componentDidMount() {
     this.socket.onopen = function (event) {
       console.log("Connected to Server!")
       //client.send("Here's some text that the server is urgently awai!"); 
     };
-    
-    // console.log("componentDidMount <App />");
-    // setTimeout(() => {
-    //   console.log("Simulating incoming message");
-    //   // Add a new message to the list of messages in the data store
-    //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    //   const messages = this.state.messages.concat(newMessage)
-    //   // Update the state of the app component.
-    //   // Calling setState will trigger a call to render() in App and all child components.
-    //   this.setState({messages: messages})
-    // }, 3000);
   }
 
+
   onNewMessage(content) {
-    const newMessage = {id: ids.generate(), username: this.state.currentUser.name, content: content}
-    const messages = this.state.messages.concat(newMessage)
-    this.setState({messages: messages})
+    // const newMessage = {id: ids.generate(), username: this.state.currentUser.name, content: content}
+    // const messages = this.state.messages.concat(newMessage)
+    // this.setState({messages: messages})
+    const newMessage = {username: this.state.currentUser.name, content: content}
+    this.socket.send(JSON.stringify(newMessage))
+    
   }
   
   onNewUsername(name) {
