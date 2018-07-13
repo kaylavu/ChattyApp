cd89233b68f77036d17f35e57098b1db510ad765
kaylavu <kaylaavu@gmail.com>
2018-07-13 06:06:06 +0000

    added text bubbles

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,7 +4,7 @@ export default function Message(props) {
     return(
       <div className="message">
         <span className="message-username" style={{color:props.color}}> {props.userName }</span>
-        <span className="message-content">{props.content}</span>
+        <span className="message-content" className='speech-bubble'>{props.content}</span>
       </div>
     )
   }
