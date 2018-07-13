9401ebf6452d03e0851de95e3979a5c2cbb81653
kaylavu <kaylaavu@gmail.com>
2018-07-13 03:07:32 +0000

    Users can add image

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -10,10 +10,20 @@ export default function MessageList(props) {
         
         switch(message.type) {
             case "incomingMessage":
-            // const wordsArray = message.content.split(' ')
-            // const wordsResult = wordsArray.filter(word => )
-            // console.log(wordsArray)
-                return <Message key={message.id} userName={message.username} content={message.content} color= {message.color} />
+            const wordsArray = message.content.split(' ');
+            const wordsResult = wordsArray.filter(word => ! (/\.(gif|jpg|jpeg|tiff|png)$/i).test(word));
+            const  imgsResult = wordsArray.filter(word =>   (/\.(gif|jpg|jpeg|tiff|png)$/i).test(word));
+            const wordsResultJoined = wordsResult.join(' '); 
+            const imgsResultArray = imgsResult.map((imgSource) => {return <img className="images" src={imgSource}/>})
+            //console.log(wordsArray)
+            if(imgsResultArray.length > 0) {
+                return <div> <Message key={message.id} userName={message.username} content={wordsResultJoined} color= {message.color} />
+                {imgsResultArray} </div>
+            } else {
+                return <Message key={message.id} userName={message.username} content={wordsResultJoined} color= {message.color} />
+               
+            }
+                
             case "incomingNotification": 
                 return <div className="notification" key={message.id}>
                 <span className="notification-content"> {message.content} </span>
